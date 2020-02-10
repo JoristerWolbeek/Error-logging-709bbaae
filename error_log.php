e987aba2ea84ec1e52e32c88de084a042b736537
Joris ter Wolbeek <21074092@talnet.nl>
2020-02-10 14:06:53 +0100

    Uitwerking van de exercise

---
+++ error_log.php
@@ -0,0 +1,7 @@
+Exception: er is een te groot getal gegeven. in C:\xampp\htdocs\my_error.php:25
+Stack trace:
+#0 C:\xampp\htdocs\my_error.php(20): countDown('1000')
+#1 {main}Exception: er is een te klein getal gegeven. in C:\xampp\htdocs\my_error.php:26
+Stack trace:
+#0 C:\xampp\htdocs\my_error.php(20): countDown('-12')
+#1 {main}
